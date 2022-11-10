202495165a1491ff0bea4e5f521034d772993b06
Henry Spivey <henryspivey2@gmail.com>
2022-11-10 09:34:03 -0800

    updates

--- myApp.js
+++ myApp.js
@@ -10,6 +10,14 @@ const makeFilePath = (template_dir, name) => {
 
 app.use('/public', express.static(__dirname+'/public'))
 
+app.all('*',(req, res, next)=>{
+    const method = req.method
+    const path = req.path 
+    const ip = req.ip     
+    console.log(`${method} ${path} - ${ip}`)
+    next()
+})
+
 app.get('/', (req, res)=> {
     res.sendFile(makeFilePath(templates_dir, 'index'))
 })
